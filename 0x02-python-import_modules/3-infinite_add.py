384a61660e4cd94bff442071e31b953d10627d59
Sylvester96 <sylvesterkbr@gmail.com>
2022-08-25 23:47:37 +0300

    corrected error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv)
-    argv = sys.argv
-    res = 0
-
-    for i in range(1, argc):
-        res = res + int(argv[i])
-
-    print(res)
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
