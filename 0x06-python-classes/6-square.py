386dadd5a8f2eca2032ee3e7a38cf2ebb120ad6a
Sylvester96 <sylvesterkbr@gmail.com>
2022-09-15 01:38:18 +0300

    corrected file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,88 +1,52 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+""" Square class """
 
 
 class Square:
-    """Represents a square
+    """ empty class Square that defines a square
     Attributes:
-        __size (int): size of a size of the square
-        __position (tuple): position of the square in 2D space
+        size: size of the square
+        position: square margins
     """
-    def __init__(self, size=0, position=(0, 0)):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-            position (tuple): positoin of the square in 2D space
-        Returns:
-            None
-        """
-        self.size = size
-        self.position = position
+    __size = 0
+    __position = 0
+
+    def __init__(self, prmSize=0, prmPosition=(0, 0)):
+        self.size = prmSize
+        self.position = prmPosition
 
     def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
+        return self.__size ** 2
 
     @property
     def size(self):
-        """getter of __size
-        Returns:
-            The size of the square
-        """
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """setter of __size
-        Args:
-            value (int): size of a side of the square
-        Returns:
-            None
-        """
-        if type(value) is not int:
+    def size(self, prmSize=0):
+        if not isinstance(prmSize, int):
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        elif prmSize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = prmSize
 
     def my_print(self):
-        """prints the square
-        Returns:
-            None
-        """
-        if self.__size == 0:
+        if self.size == 0:
             print()
-            return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print("".join([" " for k in range(self.__position[0])]), end="")
-            print("".join(["#" for l in range(self.__size)]))
+        else:
+            for row in range(self.position[1]):
+                print()
+            for column in range(self.size):
+                print("{}{}".format(" " * self.position[0], "#" * self.size))
 
     @property
     def position(self):
-        """getter of __position
-        Returns:
-            The position of the square in 2D space
-        """
         return self.__position
 
     @position.setter
-    def position(self, value):
-        """setter of __position
-        Args:
-            value (tuple): position of the square in 2D space
-        Returns:
-            None
-        """
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
+    def position(self, prmPosition):
+        if (not isinstance(prmPosition, tuple) or
+                list(map(type, prmPosition)) != [int, int] or
+                sum(0 if i >= 0 else 1 for i in prmPosition)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = prmPosition
