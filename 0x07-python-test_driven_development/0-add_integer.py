e7982f464b2358b0c983c9d321eb486d01a3a407
Sylvester96 <sylvesterkbr@gmail.com>
2022-09-23 16:10:12 +0300

    corrected error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" add_integer module """
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
+"""
 
 
-def add_integer(prmA, prmB=98):
-    """ add_integer function
-    this function add two integer
-    Attributes:
-        prmA: first integer
-        prmB: optional second integer (init by 98 by default)
-    """
-    if isinstance(prmA, float):
-        prmA = int(prmA)
-    if isinstance(prmB, float):
-        prmB = int(prmB)
-    if not isinstance(prmA, int):
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(prmB, int):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return prmA + prmB
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
