124be87ed1bcfa64d1f941384bd2ac1742241e22
Sylvester96 <sylvesterkbr@gmail.com>
2022-09-28 22:05:15 +0300

    corrected error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""Module 7.
+Writes an Object to a text file,
+using a JSON representation.
 """
-Contains the "save_to_json_file" function
-"""
+
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    """Writes the representation of my_obj
+    to filename.
+    Args:
+        - my_obj: object to write
+        - filename: file to write into
+    """
+
+    with open(filename, 'w+') as f:
         json.dump(my_obj, f)
