54dccabdc1564be9f47159dce1d4ca38aec9c0ee
Sylvester96 <sylvesterkbr@gmail.com>
2022-09-28 22:23:51 +0300

    corrected error

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,40 @@
 #!/usr/bin/python3
-""" script that count request by web request status """
-
-
+"""
+reads stdin line by line and computes metrics
+"""
 import sys
-import re
-
 
 file_size = 0
-status_count = {
-    200: 0, 301: 0, 400: 0, 401: 0,
-    403: 0, 404: 0, 405: 0, 500: 0
-}
-
-
-def incrementErrorStatusCount(prmString):
-    """ increment request number """
-    global file_size, status_count
-    try:
-        data = re.split("(.*) (.*) ([0-9]*)$", prmString)
-        if len(data) == 5:
-            if data[-3].isnumeric():
-                errorCode = int(data[-3])
-            file_size += (int(data[-2]))
-        if len(data) == 5 and checkValidity(errorCode) is True:
-            status_count[errorCode] += 1
-
-    except:
-        pass
-
-
-def checkValidity(prmErrorCode):
-    """ check error code validity """
-    global status_count
-    if prmErrorCode in status_count:
-        return True
-    return False
-
-
-def printStatistics():
-    """ print statistics """
-    global file_size, status_count
-    print("File size: {:d}".format(file_size))
-    for errorCode, count in sorted(status_count.items()):
-        if status_count[errorCode]:
-            print("{:d}: {:d}".format(errorCode, count))
-
-
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
 try:
-    for index, line in enumerate(sys.stdin):
-        incrementErrorStatusCount(line)
-        if (index + 1) % 10 == 0:
-            printStatistics()
+    for line in sys.stdin:
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+
 except KeyboardInterrupt:
-    pass
-finally:
-    printStatistics()
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
